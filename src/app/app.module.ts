68a3363856851d448a85bb9612f9e9dacbbedf1b
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-07-25 22:18:42 -0300

    front 25-07

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,55 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SocialComponent } from './components/social/social.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { HysComponent } from './components/hys/hys.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component'
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoapComponent,
+    HeaderComponent,
+    BannerComponent,
+    SocialComponent,
+    AcercadeComponent,
+    HysComponent,
+    EducacionComponent,
+    FooterComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    HomeComponent,
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
